493ebb14146d48160906c0eb2ea80abeea4d7a5c
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2020-09-04 18:50:19 +0900

    rspgame_class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const rspCoords = {
+  가위: "-142px",
+  바위: 0,
+  보: "-284px",
+};
+
+const score = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  if (imgCoord === rspCoords.가위) {
+    return "가위";
+  } else if (imgCoord === rspCoords.바위) {
+    return "바위";
+  } else if (imgCoord === rspCoords.보) {
+    return "보";
+  }
+};
+
+class RSPgame extends Component {
+  state = {
+    result: "",
+    score: 0,
+    imgCoord: 0,
+  };
+
+  interval;
+
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.바위) {
+      this.setState({ imgCoord: rspCoords.가위 });
+    } else if (imgCoord === rspCoords.가위) {
+      this.setState({ imgCoord: rspCoords.보 });
+    } else if (imgCoord === rspCoords.보) {
+      this.setState({ imgCoord: rspCoords.바위 });
+    }
+  };
+  componentDidMount() {
+    this.interval = setInterval(this.changeHand, 100);
+  }
+
+  onClickBtn = (choice) => () => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = score[choice];
+    const cpuScore = score[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
+      this.setState({ result: "비겼습니다" });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: "이겼습니다!",
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: "졌습니다!",
+          score: prevState.score - 1,
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 1000);
+  };
+
+  componentWillUnmount() {}
+
+  render() {
+    const { result, score, imgCoord } = this.state;
+    return (
+      <>
+        <div
+          id="computer"
+          style={{
+            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+          }}
+        ></div>
+        <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
+          바위
+        </button>
+        <button id="scissors" className="btn" onClick={this.onClickBtn("가위")}>
+          가위
+        </button>
+        <button id="paper" className="btn" onClick={this.onClickBtn("보")}>
+          보
+        </button>
+        <div id="result">{result}</div>
+        <div>현재 {score}점</div>
+      </>
+    );
+  }
 }
 
-export default App;
+export default RSPgame;
